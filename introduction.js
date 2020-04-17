766a6755b7cb913db37ad0070a9295960b411b25
Ayush Shrivastav <shrivastavayush32@gmail.com>
2020-04-17 21:48:14 +0530

    Introduction to JavScript

---
+++ introduction.js
@@ -0,0 +1,24 @@
+                                                   JavaScript
+
+JavaScript is a programming language that powers the dynamic behavior on most websites. Alongside HTML and CSS, it is a core technology that makes the web run. It is a scripting language.
+
+-- Console.log()
+The console.log() method is used to log or print messages to the console. It can also be used to print objects and other info.
+Important: Console here is an Object. We shall discuss about these Objects later.
+Example: 
+console.log('Hi there!');
+// Prints: Hi there!
+
+
+-- Methods in JS
+Methods return information about an object, and are called by appending an instance with a period "." , the method name, and parentheses.
+
+// Returns a number between 0 and 1
+Math.random();
+
+
+-- Libraries in JS
+Libraries contain methods that can be called by appending the library name with a period ., the method name, and a set of parentheses.
+
+ Math.random();
+//☝️ Math is the library
