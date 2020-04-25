3e3051aad0e9a55f6bf273e368e0420298939add
Ayush Shrivastav <shrivastavayush32@gmail.com>
2020-04-25 21:52:56 +0530

    Updated Functions in Js

--- functions.js
+++ functions.js
@@ -22,3 +22,44 @@ Inputs to functions are known as parameters when a function is declared or defin
 function sayHello(name) {
   return `Hello, ${name}!`;
 }
+
+
+-- Function Declaration
+Function declarations are used to create named functions. These functions can be called using their declared name.
+ Function declarations are built from:
+-The function keyword.
+-The function name.
+
+An optional list of parameters separated by commas enclosed by a set of parentheses ().
+A function body enclosed in a set of curly braces {}.
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+
+-- Calling Functions
+Functions can be called, or executed, elsewhere in code using parentheses following the function name. When a function is called, the code inside its function body runs.
+ Arguments are values passed into a function when it is called.
+
+// Defining the function
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+// Calling the function
+sum(2, 4); // 6
+
+
+-- return Keyword
+Functions return (pass back) values using the return keyword. return ends function execution and returns the specified value to the location where it was called. A common mistake is to forget the return keyword, in which case the function will return undefined by default.
+
+// With return
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+// Without return, so the function doesn't output the sum
+function sum(num1, num2) {
+  num1 + num2;
+}
+
