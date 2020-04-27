09242691da3edba3d2492ae86a24b7fcf904845d
Ayush Shrivastav <shrivastavayush32@gmail.com>
2020-04-27 23:55:03 +0530

    Updated scope.js

--- scope.js
+++ scope.js
@@ -1,4 +1,4 @@
-                                                            Scope of Variable
+                                                         Scope of Variable
 
 Scope is a concept that refers to where values and functions can be accessed.
 
@@ -21,3 +21,35 @@ function myFunction() {
 }
 
 // Code here can't use pizzaName
+
+
+-- Global Variables
+JavaScript variables that are declared outside of blocks or functions can exist in the global scope, which means they are accessible throughout a program. Variables declared outside of smaller block or function scopes are accessible inside those smaller scopes.
+
+Note: It is best practice to keep global variables to a minimum.
+
+// Variable declared globally
+const color = 'blue';
+
+function printColor() {
+  console.log(color);
+}
+
+printColor(); // Prints: blue
+
+
+-- Block Scoped Variables
+const and let are block scoped variables, meaning they are only accessible in their block or nested blocks. In the given code block, trying to print the statusMessage using the console.log() method will result in a ReferenceError. It is accessible only inside that if block.
+
+const isLoggedIn = true;
+
+if (isLoggedIn == true) {
+  const statusMessage = 'User is logged in.';
+}
+
+console.log(statusMessage);
+
+// Uncaught ReferenceError: statusMessage is not defined
+
+--Note: We should not use global scope unless required as it is a bad practice.
+  Scope should be limited till the variable is used. Also it makes the code looks clean and increases readability for other Developers.
