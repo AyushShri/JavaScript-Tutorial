8d8d9c1b471768958f22caf9ae2265a30daf948f
Ayush Shrivastav <shrivastavayush32@gmail.com>
2020-04-30 23:11:30 +0530

    Updated Arrays.js

---
+++ arrays.js
@@ -0,0 +1,18 @@
+                                                           Arrays
+Arrays are lists of ordered, stored data. They can hold items that are of any data type. 
+Arrays are created by using square brackets, with individual elements separated by commas.
+
+// An array containing numbers
+const numberArray = [0, 1, 2, 3];
+
+// An array containing different data types
+const mixedArray = [1, 'chicken', false];
+Index
+Array elements are arranged by index values, starting at 0 as the first element index. Elements can be accessed by their index using the array name, and the index surrounded by square brackets.
+
+// Accessing an array element
+const myArray = [100, 200, 300];
+
+console.log(myArray[0]); // 100
+console.log(myArray[1]); // 200
+console.log(myArray[2]); // 300
